8df33370bc51b123c83551248ca1a5ae23f175e9
luc99hen <1813927768@qq.com>
2022-03-14 20:53:30 +0800

    add sequential batch script

--- run_batch.sh
+++ run_batch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-start=2021-01-28
-end=2021-02-01
+start=2021-01-08
+end=2021-01-09
 while ! [[ "$start" > "$end" ]]; do
     next=$(date -d "$start + 1 day" +%F)
-    echo $start $next
+    start_month=$(echo $start | cut -d'-' -f2)
+    start_day=$(echo $start | cut -d'-' -f3)
+    end_month=$(echo $next | cut -d'-' -f2)
+    end_day=$(echo $next | cut -d'-' -f3)
+    sudo docker run --user root -v /media/dl/resource/luc:/wrf/host --name "case_${start_month}_${start_day}" -e START_M=$start_month -e START_D=$start_day -e END_M=$end_month -e END_D=$end_day run_case
     start=$next
 done
